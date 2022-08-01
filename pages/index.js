7846f54da16b41aa693b265d2b298d1b0a1ef3fd
Mik3D88 <maykepnh@gmail.com>
2022-08-01 02:34:47 -0300

    v1.0.1

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,7 @@ export default function Home() {
   return (
     <>
         <div className={styles.container}>
-          <h1 className={styles.titulo}>Mike Automation RPA Site </h1>
-          <button className={styles.button}>Iniciar automação de busca de conteudos</button>
-          <h3 className={styles.titulo}>Plataforma de serviços custmizados de mineração de dados </h3>
+          <h1>Automação</h1>
         </div>
     </>
   )
